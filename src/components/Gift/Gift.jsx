ffa30ed9b704511dde9546e0ad5a0dc8435065a8
Lalo0731 <l.eduardo.reyesrodriguez@gmail.com>
2026-07-19 17:44:27 -0600

    Ajustando la atrjeta de regalos

--- src/components/Gift/Gift.jsx
+++ src/components/Gift/Gift.jsx
@@ -1,42 +1,47 @@
+import { useState } from "react";
 import "../../styles/components/Gift/gift.scss";
 import butterfly from "../../assets/images/decorations/butterfly-1.png";
 
 export default function Gift() {
-  return (
-    <section className="gift">
+     const [open, setOpen] = useState(false);
+     return (
+          <section className="gift">
 
-      <div className="present">
+               <div 
+                    className={`present ${open ? "open" : ""}`}
+                    onClick={() => setOpen(!open)}
+               >
+                    <div className="gift__hint">
+                         Toca para abrir
+                    </div>
 
-        {/* 🎁 TAPA */}
-        <div className="lid">
-          <span></span>
-        </div>
+                    <div className="lid">
+                         <span></span>
+                    </div>
 
-          <div className="gift__message">
-    <h3>Con cariño 💙</h3>
-    <p>
-      Tu presencia es mi mejor regalo,<br />
-      pero si deseas tener un detalle...
-    </p>
-  </div>
+               <div className="gift__message">
+                    <h3>Con cariño 💙</h3>
+                    <p>
+                         Tu presencia es mi mejor regalo,<br />
+                         pero si deseas tener un detalle...
+                    </p>
+               </div>
 
-        {/* 🦋 CONTENIDO (mariposas) */}
-        <div className="gift__inside">
-          <img src={butterfly} className="b b1" />
-          <img src={butterfly} className="b b2" />
-          <img src={butterfly} className="b b3" />
-          <img src={butterfly} className="b b4" />
-          <img src={butterfly} className="b b5" />
-        </div>
+               <div className="gift__inside">
+                    <img src={butterfly} className="b b1" />
+                    <img src={butterfly} className="b b2" />
+                    <img src={butterfly} className="b b3" />
+                    <img src={butterfly} className="b b4" />
+                    <img src={butterfly} className="b b5" />
+               </div>
 
-        {/* 🎁 CAJA */}
-        <div className="box">
-          <span></span>
-          <span></span>
-        </div>
+               <div className="box">
+                    <span></span>
+                    <span></span>
+               </div>
 
-      </div>
+               </div>
 
-    </section>
-  );
+          </section>
+     );
 }
